6aef64f94f6ac23c6014d7939761fdd73007a5f3
javs1 <59986245+javs1@users.noreply.github.com>
2021-12-06 17:52:16 -0600

    Bowling game finished
    Co-authored-by: YxMARCKxY <mramirez73@ucol.mx>
    Co-authored-by: Villaedi <ediberto.sanchez.villalobos@gmail.com>

--- bowling.rb
+++ bowling.rb
@@ -1,19 +1,54 @@
 # frozen_string_literal: true
 
 #Class to start bwoling game to fill an array with the results of 2 shots
-class Roll
-    def tirar (tiro)
-    @shot_1 = 0
-    @shot_2 = 0
-    @score_board = Array.new(10)
-
-    if tiro==1
-      @shot_1= rand(0..10)
-    else
-      @shot_2= rand(0..10-@shot_1)
+class Bowling
+    def init
+      @shot1 = 0
+      @shot2 = 0
+      @score_board = Array.new(10)
     end
+
+    def set (shot)
+      if shot == 1
+        @shot1 = rand(0..10)
+      else
+        @shot2 = rand(0..(10 - @shot1))
+      end
+    end
+    
+    def fill_board (board_num, bonus)
+      @score_board[board_num] = set(1)
+      puts "primer tiro: " + @score_board[board_num].to_s
+
+      @score_board[board_num] = @score_board[board_num] + set(2)
+      puts "segundo tiro: " + @score_board[board_num].to_s
+      case bonus
+        when 1
+          @score_board[board_num-1] = @score_board[board_num-1] + @shot1
+        when 2
+          @score_board[board_num-1] = @score_board[board_num-1] + @shot1 + @shot2
+      end
+      @score_board[board_num] = @score_board[board_num] + @score_board[board_num - 1] if board_num != 0
+    end
+
+    def game
+      (0...10).each do |board|
+        if @shot1 == 10
+          fill_board(board,2)
+        elsif @shot1 + @shot2 == 10
+          fill_board(board,1)
+        end
+        fill_board(board,0)
+      end
+      puts @score_board
     end
     
 end
+tiro = Bowling.new()
+tiro.init
+tiro.game
+
+
+
 
 
