3d98a9f693d6562128254fbcecc5bb09a000e4e5
javs1 <59986245+javs1@users.noreply.github.com>
2021-12-06 18:21:24 -0600

    Rubicritic initialize and corrections with rubocop
    Co-authored-by: YxMARCKxY <mramirez73@ucol.mx>
    Co-authored-by: Villaedi <ediberto.sanchez.villalobos@gmail.com>

--- bowling.rb
+++ bowling.rb
@@ -1,54 +1,49 @@
 # frozen_string_literal: true
 
-#Class to start bwoling game to fill an array with the results of 2 shots
+# Class to start bwoling game to fill an array with the results of 2 shots
 class Bowling
-    def init
-      @shot1 = 0
-      @shot2 = 0
-      @score_board = Array.new(10)
-    end
+  def init
+    @first_shot = 0
+    @second_shot = 0
+    @score_board = Array.new(10)
+  end
 
-    def set (shot)
-      if shot == 1
-        @shot1 = rand(0..10)
-      else
-        @shot2 = rand(0..(10 - @shot1))
-      end
+  def set(shot)
+    if shot == 1
+      @first_shot = rand(0..10)
+    else
+      @second_shot = rand(0..(10 - @first_shot))
     end
-    
-    def fill_board (board_num, bonus)
-      @score_board[board_num] = set(1)
-      puts "primer tiro: " + @score_board[board_num].to_s
+  end
 
-      @score_board[board_num] = @score_board[board_num] + set(2)
-      puts "segundo tiro: " + @score_board[board_num].to_s
-      case bonus
-        when 1
-          @score_board[board_num-1] = @score_board[board_num-1] + @shot1
-        when 2
-          @score_board[board_num-1] = @score_board[board_num-1] + @shot1 + @shot2
-      end
-      @score_board[board_num] = @score_board[board_num] + @score_board[board_num - 1] if board_num != 0
+  def fill_board(board_num, bonus)
+    @score_board[board_num] = set(1)
+    puts "primer tiro: #{@score_board[board_num]}"
+    @score_board[board_num] = @score_board[board_num] + set(2)
+    puts "segundo tiro: #{@score_board[board_num]}"
+    case bonus
+    when 1
+      @score_board[board_num - 1] = @score_board[board_num - 1] + @first_shot
+    when 2
+      @score_board[board_num - 1] = @score_board[board_num - 1] + @first_shot + @second_shot
     end
+    @score_board[board_num] = @score_board[board_num] + @score_board[board_num - 1] if board_num != 0
+  end
 
-    def game
-      (0...10).each do |board|
-        if @shot1 == 10
-          fill_board(board,2)
-        elsif @shot1 + @shot2 == 10
-          fill_board(board,1)
-        end
-        fill_board(board,0)
+  def game
+    (0...10).each do |board|
+      if @first_shot == 10
+        fill_board(board, 2)
+      elsif @first_shot + @second_shot == 10
+        fill_board(board, 1)
       end
-      puts @score_board
+      fill_board(board, 0)
+      puts "Resultado: #{@score_board[board]}\n"
+      puts "RONDA #{board + 1}\n\n"
     end
-    
+  end
 end
-tiro = Bowling.new()
+
+tiro = Bowling.new
 tiro.init
 tiro.game
-
-
-
-
-
